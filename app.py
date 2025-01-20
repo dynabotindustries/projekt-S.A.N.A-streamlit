56a67387cbb0fbb6a5d9522ea3ad19a704107682
Anish-MutliTalent <140186055+Anish-MutliTalent@users.noreply.github.com>
2025-01-20 20:36:33 +0530

    Update app.py
    Added image in chat history (properly)

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ for sender, message in st.session_state["chat_history"]:   # Parse session chat 
             # st.image('https://avatars.githubusercontent.com/u/175069629?v=4', width=35)
         # Add title to column 2
         # with rcol2:
-        st.markdown(f"<img src='https://avatars.githubusercontent.com/u/175069629?v=4' width=20 style='display:inline-block; margin-right:20px'></img><b>S.A.N.A:</b> {message}", unsafe_allow_html=True)
+        st.markdown(f"<img src='https://avatars.githubusercontent.com/u/175069629?v=4' width=20 style='display:inline-block; margin-right:10px'></img><b>S.A.N.A:</b> {message}", unsafe_allow_html=True)
     else:
         st.markdown(f"**❗Unknown Sender:** {message}")
 
