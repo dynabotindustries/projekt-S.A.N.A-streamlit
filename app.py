f4cc88782fe7e3936f9edd6ff32a38278edda0dd
Anish-MutliTalent <140186055+Anish-MutliTalent@users.noreply.github.com>
2025-01-20 20:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ col1, mid, col2 = st.columns([1,1,20])
 with col1:
     st.image('https://avatars.githubusercontent.com/u/175069629?v=4', width=60)
 with col2:
-    st.write('Projekt S.A.N.A')
+    st.title('Projekt S.A.N.A')
 st.markdown("""
 **S.A.N.A** is a secure, autonomous, and non-intrusive virtual assistant. 
 Feel free to ask me anything! 😊
